88ffd3b1c84eb24b54ce234b0edced85d509df69
suryaKanthjunior <114057966+suryaKanthjunior@users.noreply.github.com>
2022-11-11 19:23:45 +0530

    comitted

--- sign-page/src/App.js
+++ sign-page/src/App.js
@@ -1,4 +1,3 @@
-import { logDOM } from '@testing-library/react';
 import React from 'react';
 import App from './App.css';
 import logo from './images/logo.png';
@@ -8,12 +7,15 @@ const Login = () =>{
 	return (
 		<div className='form'>
       <img src={logo}></img>
-			<form action=""> 
+			<form action="">
+				<div className='total'>
       <div  className='head'>
+	
         <header>
+			
           <h3><a>Admin Log in</a></h3>
         </header>
-      </div>
+		</div>
 				<div className='mail'> 
 					<label className="mobile" htmlFor="email"><a>Email Id or Mobile Number</a></label>
 					<input type="text" name="email" id="email" placeholder="Your mail Id or Mobile number" /> 
@@ -24,12 +26,16 @@ const Login = () =>{
 					<input type="text" name="password" id="password" placeholder="Your Password" /> 
 				</div>  
         <div className='checkbox'>
-          <input type="checkbox"></input>
+          <input className='input' type="checkbox"></input>
           <label className="remember" htmlFor="text"><a>Remember Me</a></label>
         </div>
 				<button type="submit"><a>LOG IN</a></button>
+				</div>
+				
 			</form>
-		</div>
+			
+			</div> 		
+			
 	);
 };
 
