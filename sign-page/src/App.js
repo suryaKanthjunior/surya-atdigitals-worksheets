08ab63ac8b9070a3a1ae554869d579607725e4aa
suryaKanthjunior <114057966+suryaKanthjunior@users.noreply.github.com>
2022-11-15 15:07:02 +0530

    committed

--- sign-page/src/App.js
+++ sign-page/src/App.js
@@ -1,42 +1,51 @@
-import React from 'react';
-import App from './App.css';
-import logo from './images/logo.png';
+import React from "react";
+import App from "./App.css";
+import logo from "./images/logo.png";
 
-
-const Login = () =>{ 
-	return (
-		<div className='form'>
+const Login = () => {
+  return (
+    <div className="form">
       <img src={logo}></img>
-			<form action="">
-				<div className='total'>
-      <div  className='head'>
-	
-        <header>
-			
-          <h3>Admin Log in</h3>
-        </header>
-		</div>
-				<div className='mail'> 
-					<label className="mobile" htmlFor="email">Email Id or Mobile Number</label>
-					<input type="text" name="email" id="email" placeholder="Your mail Id or Mobile number" /> 
-          
-				</div> 
-				<div className='number'> 
-					<label className="word" htmlFor="password">Password</label>
-					<input type="text" name="password" id="password" placeholder="Your Password" /> 
-				</div>  
-        <div className='checkbox'>
-          <input className='input' type="checkbox"></input>
-          <label className="remember" htmlFor="text">Remember Me</label>
+      <form action="">
+        <div className="total">
+          <div className="head">
+            <header>
+              <h3>Admin Log in</h3>
+            </header>
+          </div>
+          <div className="mail">
+            <label className="mobile" htmlFor="email">
+              Email Id or Mobile Number
+            </label>
+            <input className="one"
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Your mail Id or Mobile number"
+            />
+          </div>
+          <div className="number">
+            <label className="word" htmlFor="password">
+              Password
+            </label>
+            <input className="one"
+              type="text"
+              name="password"
+              id="password"
+              placeholder="Your Password"
+            />
+          </div>
+          <div className="checkbox">
+            <input className="input" type="checkbox"></input>
+            <label className="remember" htmlFor="text">
+              Remember Me
+            </label>
+          </div>
+          <button type="submit">LOG IN</button>
         </div>
-				<button type="submit">LOG IN</button>
-				</div>
-				
-			</form>
-			
-			</div> 		
-			
-	);
+      </form>
+    </div>
+  );
 };
 
-export default Login; 
+export default Login;
