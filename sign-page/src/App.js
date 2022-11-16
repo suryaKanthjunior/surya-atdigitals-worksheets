04b2ef556441fdaf631b286b75106dbbe114fc08
suryaKanthjunior <114057966+suryaKanthjunior@users.noreply.github.com>
2022-11-16 18:55:25 +0530

    committed

--- sign-page/src/App.js
+++ sign-page/src/App.js
@@ -1,12 +1,33 @@
 import React from "react";
 import App from "./App.css";
 import logo from "./images/logo.png";
+import { useState } from "react";
 
 const Login = () => {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredEmail, enteredPassword);
+    setEnteredEmail("");
+    setEnteredPassword("");
+  };
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
   return (
     <div className="form">
       <img src={logo}></img>
-      <form action="">
+      <form onSubmit={submitHandler}>
         <div className="total">
           <div className="head">
             <header>
@@ -17,22 +38,30 @@ const Login = () => {
             <label className="mobile" htmlFor="email">
               Email Id or Mobile Number
             </label>
-            <input className="one"
-              type="text"
-              name="email"
+            <input
+              className="one"
+              type="email"
+              value={enteredEmail}
+              onChange={emailChangeHandler}
               id="email"
               placeholder="Your mail Id or Mobile number"
+              name="email"
+              required
             />
           </div>
           <div className="number">
             <label className="word" htmlFor="password">
               Password
             </label>
-            <input className="one"
-              type="text"
-              name="password"
+            <input
+              className="one"
+              type="password"
               id="password"
+              value={enteredPassword}
+              onChange={passwordChangeHandler}
               placeholder="Your Password"
+              name="password"
+              required
             />
           </div>
           <div className="checkbox">
